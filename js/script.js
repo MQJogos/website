0ab9919da729e9cdeb1d01dd0c464d5feeef6430
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2021-12-20 16:40:06 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,32 +1,26 @@
-let menu = document.querySelector('#menu-bar');
-let navbar = document.querySelector('.navbar');
+let searchBtn = document.querySelector('#search-btn');
+let searchForm = document.querySelector('.header .search-form');
 
-menu.onclick = () =>{
+searchBtn.onclick = () =>{
+   searchBtn.classList.toggle('fa-times');
+   searchForm.classList.toggle('active');
+   menuBtn.classList.remove('fa-times');
+   navbar.classList.remove('active');
+}
 
-  menu.classList.toggle('fa-times');
-  navbar.classList.toggle('active');
+let menuBtn = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
 
+menuBtn.onclick = () =>{
+   menuBtn.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
+   searchBtn.classList.remove('fa-times');
+   searchForm.classList.remove('active');
 }
 
 window.onscroll = () =>{
-
-  menu.classList.remove('fa-times');
-  navbar.classList.remove('active');
-
-  if(window.scrollY > 60){
-    document.querySelector('#scroll-top').classList.add('active');
-  }else{
-    document.querySelector('#scroll-top').classList.remove('active');
-  }
-
+   searchBtn.classList.remove('fa-times');
+   searchForm.classList.remove('active');
+   menuBtn.classList.remove('fa-times');
+   navbar.classList.remove('active');
 }
-
-function loader(){
-  document.querySelector('.loader-container').classList.add('fade-out');
-}
-
-function fadeOut(){
-  setInterval(loader, 3000);
-}
-
-window.onload = fadeOut();
